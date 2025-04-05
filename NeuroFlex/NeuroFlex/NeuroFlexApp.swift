9fcf76bec4775e5837203277dbcea8536ddcd127
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-04-05 12:13:44 -0700

    xcode

---
+++ NeuroFlex/NeuroFlex/NeuroFlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroFlexApp.swift
+//  NeuroFlex
+//
+//  Created by Henry on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NeuroFlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
